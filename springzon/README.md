6827e523b0db712118b14dd8f7d1fa24a822870b
ddiana15 <120561318+ddiana15@users.noreply.github.com>
2022-12-15 12:56:39 +0100

    aggiunto progetto

---
+++ springzon/README.md
@@ -0,0 +1,1 @@
+# springzon
---
+++ springzon/springzon/src/main/java/com/antsrl/springzon/SpringzonApplication.java
@@ -0,0 +1,13 @@
+package com.antsrl.springzon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringzonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringzonApplication.class, args);
+	}
+
+}
