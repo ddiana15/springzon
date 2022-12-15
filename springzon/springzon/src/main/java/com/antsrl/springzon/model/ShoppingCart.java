cd68f68ce5b76696904ce265d3509697c0e0c3c2
Rachele <rachele.dedonnantonio@outlook.it>
2022-12-15 17:10:41 +0100

    Added //test comment in ShoppingCart.java

--- springzon/springzon/src/main/java/com/antsrl/springzon/model/ShoppingCart.java
+++ springzon/springzon/src/main/java/com/antsrl/springzon/model/ShoppingCart.java
@@ -1,14 +1,13 @@
 package com.antsrl.springzon.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 import java.util.List;
-
+//test
 @Entity
-@Data
+@Getter
+@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Table(name = "shopping_cart")
